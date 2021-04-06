aaad877d206f8037d87bbb3120fce5f7ede91dcc
Ritok-K <marharyta.kiselhova@nure.ua>
2021-04-06 17:57:43 +0300

    add creation list method

--- List/List/Program.cs
+++ List/List/Program.cs
@@ -4,6 +4,20 @@ namespace List
 {
     class Program
     {
+        static SortedLinkedList<int> BuildList()
+        {
+            var list = new SortedLinkedList<int>((int i1, int i2) => i1 < i2);
+
+            list.Insert(1);
+            list.Insert(0);
+            list.Insert(-2);
+            list.Insert(5);
+            list.Insert(1);
+            list.Insert(9);
+
+            return list;
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
