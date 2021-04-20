d1dc33d53f776b6ee1e6c0a1f3b5e871a5730d41
Ritok-K <marharyta.kiselhova@nure.ua>
2021-04-20 21:21:23 +0300

    add class Node, class Tree

---
+++ AVL_Tree/AVL_Tree/BBSTNode.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AVL_Tree
+{
+    public class Tree
+    {
+        public class Node
+        {
+            internal Node left;
+            internal Node right;
+
+            internal int value;
+        }
+    }
+}
