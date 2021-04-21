e2fab75f67d858248cb9df066c5f084ab5a36a06
Ritok-K <marharyta.kiselhova@nure.ua>
2021-04-21 12:59:47 +0300

    add properties to class Node

--- AVL_Tree/AVL_Tree/BBSTNode.cs
+++ AVL_Tree/AVL_Tree/BBSTNode.cs
@@ -10,10 +10,28 @@ namespace AVL_Tree
     {
         public class Node
         {
+            internal int value;
+
             internal Node left;
             internal Node right;
 
-            internal int value;
+            public int Value { get => value; }
+            public Node Left { get => left; }
+            public Node Right { get => right; }
+            public bool IsLeaf { get => left == null && right == null; }
+
+            public int Height { get => 1 + Math.Max(LeftSubTreeHeight, RightSubTreeHeight); }
+            public int LeftSubTreeHeight { get => (left != null) ? left.Height : 0; }
+            public int RightSubTreeHeight { get => (right != null) ? right.Height : 0; }
+
+            public int BalanceFactor { get => LeftSubTreeHeight - RightSubTreeHeight; }
+
+            public Node(int v)
+            {
+                value = v;
+            }
         }
+
+        Node root;
     }
 }
