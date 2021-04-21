770658914f054e180fb3d29293218a11448dc5ef
Ritok-K <marharyta.kiselhova@nure.ua>
2021-04-21 13:59:50 +0300

    check method OutputInOrder

--- AVL_Tree/AVL_Tree/Program.cs
+++ AVL_Tree/AVL_Tree/Program.cs
@@ -4,8 +4,27 @@ namespace AVL_Tree
 {
     class Program
     {
+        static Tree BuildTree()
+        {
+            var tree = new Tree();
+            tree.Add(17);
+            tree.Add(25);
+            tree.Add(30);
+            tree.Add(35);
+            tree.Add(40);
+
+            return tree;
+        }
+
+        static void OutputTree(Tree tree)
+        {
+            Console.WriteLine("Tree (in order):");
+            tree.OutputInOrder();
+        }
         static void Main(string[] args)
         {
+            var tree = BuildTree();
+            OutputTree(tree);
         }
     }
 }
