6daa12095ddd3cabaa124fea4c5b3c0fb81ed43f
Ritok-K <marharyta.kiselhova@nure.ua>
2021-05-05 20:30:24 +0300

    add HeapSort

--- ATSDlb3/ATSDlb3/Program.cs
+++ ATSDlb3/ATSDlb3/Program.cs
@@ -10,14 +10,22 @@ namespace ATSDlb3
             list.Add(2);
             list.Add(-3);
             list.Add(0);
-            list.Add(2);
+            list.Add(-6);
             list.Add(8);
+            //list.Add(20);
+            list.Add(5);
 
             Console.WriteLine("Unordered list");
-            for (int i = 0; i < list.Length; i++)
-            {
-                Console.WriteLine(list[i]);
-            }
+            list.Print();
+
+            Console.WriteLine();
+            Console.WriteLine("HeapSorted list");
+            list.HeapSort();
+            list.Print();
+
+
+
+
         }
     }
 }
