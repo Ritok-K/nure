1b22f350e1e2b70aea1da53a2bdc7d3b116bd231
Ritok-K <marharyta.kiselhova@nure.ua>
2021-05-05 17:15:19 +0300

    make constructor

---
+++ ATSDlb3/ATSDlb3/UnorderedList.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATSDlb3
+{
+    class UnorderedList
+    {
+        public UnorderedList(int maxSize)
+        {
+            nodes = new int[maxSize];
+            end = 0;
+        }
+
+        int[] nodes;
+        int end;
+    }
+}
