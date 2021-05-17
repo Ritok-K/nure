dda34050e210e60c8b7dc9f49b7fda301cb737a1
Ritok-K <marharyta.kiselhova@nure.ua>
2021-05-17 14:45:44 +0300

    add task1

--- PT2/PT2/RecursionMath.cs
+++ PT2/PT2/RecursionMath.cs
@@ -8,5 +8,23 @@ namespace PT2
 {
     class RecursionMath
     {
+        static int Prod(int a, int b)
+        {
+            return a == 0 ? 0 : b + Prod(a - 1, b);
+        }
+
+        static float Task1(int n)
+        {
+            if(n == 0)
+            {
+                return 3;
+            }
+            else if(n > 0)
+            {
+                return 4 * Task1(n - 1) + 2 * Task1(n / 2) + 7;
+            }
+
+            throw new ArgumentException();
+        }
     }
 }
